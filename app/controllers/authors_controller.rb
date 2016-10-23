919e1a906878ee36a9a8c5bab7d302afde68d09b
Sacha Andre <andresachaa@gmail.com>
2016-10-23 16:35:03 +0200

    Obligation de Log sur les deux onglets

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,5 @@
 class AuthorsController < ApplicationController
+  before_action :require_user, only: [:index, :show]
   before_action :set_author, only: [:show, :edit, :update, :destroy]
 
   # GET /authors
