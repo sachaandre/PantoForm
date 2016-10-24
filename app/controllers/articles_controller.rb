7fd3132a5cc0f60049bed1fcb233abcfad76d220
Sacha Andre <andresachaa@gmail.com>
2016-10-24 14:14:25 +0200

    Ajout Procfile

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.all.order(:fr_name)
   end
 
   # GET /articles/1
