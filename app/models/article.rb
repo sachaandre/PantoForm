b8cc6b2f0081325b10a238b39159fe062f47a5ec
Sacha Andre <andresachaa@gmail.com>
2016-10-29 10:00:59 +0200

    Mise en Forme Theme#Index

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,8 +5,6 @@ class Article < ApplicationRecord
   has_many :article_themes
   has_many :themes, through: :article_themes
 
-  accept_nested_attributes_for :theme
-
   def gather_theme
     self.theme_ids = [theme_one_id, theme_two_id, theme_three_id]
   end
