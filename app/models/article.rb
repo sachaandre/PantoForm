a13a8b01545c403eb9005af7430dc51e59b74244
Sacha Andre <andresachaa@gmail.com>
2016-11-30 12:59:51 +0100

    Designing blocks ~ Designing titlebar

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,4 +11,22 @@ class Article < ApplicationRecord
     self.save!
   end
 
+  def random_color
+    rdm = rand(5)
+    case rdm
+    when 0
+      clr="border-top-red"
+    when 1
+      clr="border-top-orange"
+    when 2
+      clr="border-top-green"
+    when 3
+      clr="border-top-purple"
+    when 4
+      clr="border-top-blue"
+    else
+      clr="border-top-grey"
+    end
+  end
+
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -19,4 +19,21 @@ class Author < ApplicationRecord
     email == ADMIN_MAIL
   end
 
+  def random_color
+    rdm = rand(5)
+    case rdm
+    when 0
+      clr="border-top-red"
+    when 1
+      clr="border-top-orange"
+    when 2
+      clr="border-top-green"
+    when 3
+      clr="border-top-purple"
+    when 4
+      clr="border-top-blue"
+    else
+      clr="border-top-grey"
+    end
+  end
 end
